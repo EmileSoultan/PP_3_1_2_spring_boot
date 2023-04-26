f391c6081e8aa2215e9d753e682e878318d09d34
Emile <emabish42@gmail.com>
2023-04-26 19:45:50 +0200

    Initial commit

---
+++ src/main/java/kata/academy/spring_boot/SpringBootApplication.java
@@ -0,0 +1,10 @@
+package kata.academy.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
