ccb8de5c676f86dead581e0a18ee41d7ea78ef26
Emile <emabish42@gmail.com>
2023-04-28 10:38:26 +0200

    Удалил и перенес аннотацию @Transactional в UserService

--- src/main/java/kata/academy/spring_boot/dao/UserDAOJpaImpl.java
+++ src/main/java/kata/academy/spring_boot/dao/UserDAOJpaImpl.java
@@ -3,13 +3,11 @@ package kata.academy.spring_boot.dao;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import kata.academy.spring_boot.models.User;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
-@Transactional(readOnly = true)
+@Repository
 public class UserDAOJpaImpl implements UserDAO {
     @PersistenceContext
     private EntityManager entityManager;
@@ -24,13 +22,11 @@ public class UserDAOJpaImpl implements UserDAO {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
     @Override
     public void save(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
     @Override
     public void update(long id, User user) {
         User userToBeUpdated = entityManager.find(User.class, id);
@@ -39,7 +35,6 @@ public class UserDAOJpaImpl implements UserDAO {
         userToBeUpdated.setEmail(user.getEmail());
     }
 
-    @Transactional
     @Override
     public void delete(long id) {
         User user = entityManager.find(User.class, id);
